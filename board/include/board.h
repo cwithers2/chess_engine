dfe6e15368bacf80ba389ee5b1278a60a48d0acb
colin <cwithers.dev@gmail.com>
2024-02-25 12:19:28 -0600

    Wrote various files for libboard.a: -board.h (user interface) -board.c (library implementation, currently empty) -board_types.h (seperate file to define data types) -test.c (run unit tests, currently empty) -Makefile: build, test, and clean

---
+++ board/include/board.h
@@ -0,0 +1,12 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <board_types.h>
+
+Board* board_init();
+void board_destroy(Board* board);
+int board_set(Board* board, char* fen);
+void board_copy(Board* src, Board* dest);
+int board_list(Board* board, BoardColor color, int* count, BoardMove** moves);
+int board_move(BoardMove* move);
+
+#endif
