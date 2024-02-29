11ccc9d4d20da03dc25b0e6531a2cf949acdbe2b
colin <cwithers.dev@gmail.com>
2024-02-29 00:38:13 -0600

    Wrote unit tests for tokenizer functions

--- engine/src/test.c
+++ engine/src/test.c
@@ -1,15 +1,88 @@
-#include "test.h"
+#include <test.h>
 
-int test_pass(){
-	return 1;
+//tokenizer tests
+#include <tokenize.h>
+int test1(){
+	char buffer[] = "Hello, World!";
+	char delim[] = "j, t";
+	return count_until_delim(buffer, delim) == 5;
 }
 
-int test_fail(){
-	return 0;
+int test2(){
+	char buffer[] = "Hello, World!";
+	char delim[] = "HeloW ";
+	return count_while_delim(buffer, delim) == 5;
+}
+
+int test3(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", ";
+	char token[] = "Hello";
+	return check_token(buffer, token, delim);
+}
+
+int test4(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", ";
+	char token[] = "World!";
+	return !check_token(buffer, token, delim);
+}
+
+int test5(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", ";
+	char token[] = "Hello";
+	return count_until_token(buffer, token, delim) == 0;
+}
+
+int test6(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", ";
+	char token[] = "World";
+	return count_until_token(buffer, token, delim) < 0;
+}
+
+int test7(){
+	char buffer[] = "  ,,  ,,, ,  Hello, World!";
+	char delim[] = ", ";
+	return count_while_delim(buffer, delim) == 13; 
+}
+
+int test8(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", ";
+	return count_while_delim(buffer, delim) == 0;
+}
+
+int test9(){
+	char buffer[] = " Hello, World!";
+	char delim[] = ", ";
+	return count_until_delim(buffer, delim) == 0;
+}
+
+int test10(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", !";
+	char token[] = "World";
+	return count_until_token(buffer, token, delim) == 7;
+}
+
+int test11(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", !";
+	char token[] = "Hello";
+	return count_until_token(buffer, token, delim) == 0;
+}
+
+int test12(){
+	char buffer[] = "Hello, World!";
+	char delim[] = ", !";
+	char token[] = "Goodbye";
+	return count_until_token(buffer, token, delim) == -13;
 }
 
 TEST_GENERATE(
-	test_pass,
-	test_fail,
-	test_pass // never reached
+	//tokenizer tests
+	test1, test2, test3, test4,  test5,  test6,
+	test7, test8, test9, test10, test11, test12
 );
