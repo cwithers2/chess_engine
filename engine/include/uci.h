f81d8dab3784f8240a09c9179ee5c6a44de80758
colin <cwithers.dev@gmail.com>
2024-03-07 17:09:03 -0600

    Rewrote parser symantics. Debugging required.

--- engine/include/uci.h
+++ engine/include/uci.h
@@ -1,14 +1,18 @@
 #ifndef UCI_H
 #define UCI_H
 
-#ifndef UCI_INPUT_BUFFER_LEN
-#define UCI_INPUT_BUFFER_LEN 256
-#endif
+typedef enum UCI_PARSE_MODE UCI_PARSE_MODE;
+
+enum UCI_PARSE_MODE{
+	UCI_PARSE_INPUT,
+	UCI_PARSE_OUTPUT
+};
 
 int uci_init();
 void uci_destroy();
 
-int uci_poll(int* argc, char*** argv);
-int uci_respond(int argc, char** argv);
+int uci_write(char* fmt, ...);
+int uci_parse(char* buffer, int* argc, char*** argv, UCI_PARSE_MODE mode);
 
+void uci_free_vector(int argc, char** argv);
 #endif
