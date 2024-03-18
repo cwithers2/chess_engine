57d2c2ac0276812343d70581235fd28e6fb0b011
colin <cwithers.dev@gmail.com>
2024-03-18 17:57:49 -0500

    Forward-declared essential functions and datatypes.

--- engine/board/include/board.h
+++ engine/board/include/board.h
@@ -1,4 +1,12 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+typedef struct Board Board;
+typedef union BoardMove BoardMove;
+
+int board_new(Board* board, const char* fen);
+int board_copy(Board* board, Board* copy);
+int board_list(Board* board, int* argc, BoardMove** argv);
+int board_play(Board* board, BoardMove* move);
+void board_free_moves(int argc, BoardMove* argv);
 #endif
--- engine/board/src/board.c
+++ engine/board/src/board.c
@@ -1,1 +1,22 @@
 #include <board.h>
+
+struct Board{
+};
+
+union BoardMove{
+};
+
+int board_new(Board* board, const char* fen){
+}
+
+int board_copy(Board* board, Board* copy){
+}
+
+int board_list(Board* board, int* argc, BoardMove** argv){
+}
+
+int board_play(Board* board, BoardMove* move){
+}
+
+void board_free_moves(int argc, BoardMove* argv){
+}
